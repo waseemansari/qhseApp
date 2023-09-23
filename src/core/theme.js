23ca7f80743f1797dc61f414895b2a9c7eb8d445
waseemansari <waseem0320@gmail.com>
2023-09-23 12:29:29 +0500

    my first commit

---
+++ src/core/theme.js
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#bc9c50',
+    secondary: '#414757',
+    white: '#ffffff',
+    error: '#f13a59',
+    
+  },
+  bgColor: '#000000',
+  
+  
+}
---
+++ src/screens/StartScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>WELCOME</Header>
+      
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+    </Background>
+  )
+}
